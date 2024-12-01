cddc42a41284c79af64d9f641bc8469bfbae0963
KiranMantha <kiranv.mantha@gmail.com>
2024-12-01 18:57:20 +0530

    added prometheus monitoring for deno hono endpoints

--- backend-app/deno/main.ts
+++ backend-app/deno/main.ts
@@ -1,8 +1,27 @@
-import { Hono } from "hono";
+import { Context, Hono } from "hono";
 import { runOperationInIsolate } from "./runOperationInIsolate.ts";
+import { prometheus } from "@hono/prometheus";
 
 const port = 3000;
 const app = new Hono();
+const { printMetrics, registerMetrics } = prometheus({
+  prefix: "hono_app_",
+  metricOptions: {
+    requestDuration: {
+      customLabels: {
+        api_operation: (c: Context) => c.req.routePath === '/metrics' ? 'metrics' : c.req.param("operation") ?? ''
+      }
+    },
+    requestsTotal: {
+      customLabels: {
+        api_operation: (c: Context) => c.req.routePath === '/metrics' ? 'metrics' : c.req.param("operation") ?? ''
+      }
+    },
+  },
+});
+
+app.use("*", registerMetrics);
+app.get("/metrics", printMetrics);
 
 // Sample endpoint: Accept operation and payload
 app.post("/api/:operation", async (c) => {
@@ -14,7 +33,7 @@ app.post("/api/:operation", async (c) => {
 
 app.get("/api/:operation", async (c) => {
   const operation = c.req.param("operation");
-  const name = c.req.query('name');
+  const name = c.req.query("name");
   const result = await runOperationInIsolate(operation, { name });
   return c.json({ result: JSON.parse(result) });
 });
